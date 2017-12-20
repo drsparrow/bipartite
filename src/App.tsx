1e4a5c50c320a755582b9c425a7e5898c5573c94
Ari Brenner <aribrenner@gmail.com>
2017-12-20 00:13:32 -0500

    more links

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,13 @@ const sources = ['a', 'b', 'c', 'd', 'e'];
 const targets = ['w', 'x', 'y', 'z'];
 const links = [
   {source: 0, target: 0, value: 2},
-  {source: 0, target: 1, value: 3},
+  {source: 0, target: 1, value: 2},
   {source: 1, target: 2, value: 4},
+  {source: 1, target: 3, value: 3},
   {source: 2, target: 1, value: 2},
-  {source: 3, target: 2, value: 1},
+  {source: 2, target: 2, value: 3},
+  {source: 3, target: 2, value: 2},
+  {source: 3, target: 0, value: 1},
   {source: 4, target: 0, value: 4},
   {source: 4, target: 1, value: 1},
   {source: 4, target: 2, value: 2},
