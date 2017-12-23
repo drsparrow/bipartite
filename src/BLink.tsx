22929130014bbf695abfb248382d69ffd215d9f8
Ari Brenner <aribrenner@gmail.com>
2017-12-23 17:25:19 -0500

    linearGradient not appearing for straight curves

--- src/BLink.tsx
+++ src/BLink.tsx
@@ -18,7 +18,9 @@ export default function BLink (props: Props) {
   const midX = (x1 + x2) / 2;
   const midX1 = midX + (tightness * midX);
   const midX2 = midX - (tightness * midX);
-  const d = `M${x1},${y1} C${midX1},${y1} ${midX2},${y2} ${x2},${y2}`;
+
+  const iota = y1 === y2 ? 0.001 : 0; // hack. linearGradient does not work with straight lines
+  const d = `M${x1},${y1} C${midX1},${y1+iota} ${midX2},${y2+iota} ${x2},${y2}`;
 
   return (
     <path
